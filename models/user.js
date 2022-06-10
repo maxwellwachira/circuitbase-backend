1b3bd68c28666054762bf337159530c04c675a80
maxwellwachira <maxwellwachira67@gmail.com>
2022-06-10 13:31:47 +0300

    User schema

---
+++ models/user.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    id: {type: String},
+});
+
+
+export default mongoose.model("User", userSchema);
