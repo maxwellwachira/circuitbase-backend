2f8cce21c9aeed9593b63894717ee32ba3b1f321
maxwellwachira <maxwellwachira67@gmail.com>
2022-06-10 13:29:28 +0300

    application entry point

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import userRoutes from './routes/users.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/user', userRoutes);
+
+const CONNECTION_URL = process.env.MONGODB_STRING;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('bufferCommands', false);
+
